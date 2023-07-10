dbf8d9fa4d465d6b6a4cd8d85d1a7d1769d476f7
John Cristiani <jcristia10@gmail.com>
2023-07-10 14:04:44 -0700

    edgebundle

--- dataprep.py
+++ dataprep.py
@@ -1,4 +1,56 @@
 
+# Testing out edge bundling with the MPA connections
+
 
 # https://datashader.org/user_guide/Networks.html#edge-rendering-bundling
 # https://stackoverflow.com/questions/61333273/how-to-use-edge-bundling-with-networkx-and-matplotlib-in-python
+
+
+import arcpy
+import os
+import pandas as pd
+
+root = r'C:\Users\cristianij\Documents\Projects\edgebundling'
+mpa_pts = os.path.join(root, 'spatial/COMBINED.gdb/patch_centroids')
+exclude = os.path.join(root, 'spatial/mpas.gdb/M10_toexcludefromanalysis')
+conns = os.path.join(root, 'spatial/COMBINED.gdb/conn_avg_pld{}')
+plds = ['1', '3', '7', '10', '21', '30', '40', '60']
+
+
+# add XY fields to patch centroids
+fields = arcpy.ListFields(mpa_pts)
+field_names = [field.name for field in fields]
+if 'POINT_X' not in field_names:
+    arcpy.AddXY_management(mpa_pts)
+
+# read in as pandas df
+cursor = arcpy.da.SearchCursor(mpa_pts, ['uID_202011', 'POINT_X', 'POINT_Y']) 
+df = pd.DataFrame(data=[row for row in cursor], columns=['name', 'x', 'y'])
+
+# exclude MPAs where oceaonographic models are not resolved
+cursor = arcpy.da.SearchCursor(exclude, ['uID_20201124', 'exclude']) 
+df_ex = pd.DataFrame(data=[row for row in cursor], columns=['name', 'exclude'])
+df = df.merge(df_ex, on='name')
+df = df[df.exclude.isna()]    # LOOK UP WHAT I DID IN THE PAST  . ALSO REMOVE EXCLUDE COLUMN
+# list of uIDs to exclude
+ex_list = list(df_ex.name[df_ex.exclude.isin([1,2])])
+
+df.to_csv(os.path.join(root, 'spatial/nodes.csv'))
+
+
+
+# connections to df to csv
+for pld in plds:
+    
+    # read in as df
+    conn = conns.format(pld)
+    cursor = arcpy.da.SearchCursor(conn, ['from_id', 'to_id', 'prob_avg']) 
+    df = pd.DataFrame(data=[row for row in cursor], columns=['source', 'target', 'weight'])
+
+    # exclude certain mpas
+    df = df[(~df.target.isin(ex_list)) & (~df.source.isin(ex_list))]
+
+    # exclude self connections
+    df = df[df.source != df.target]
+
+    df.to_csv(os.path.join(root, f'spatial/edges_pld{pld}.csv'))
--- edgebundling.py
+++ edgebundling.py
@@ -1,5 +1,67 @@
 
+# Testing out edge bundling with the MPA connections
+# See discussion notes below and why I paused working on this.
+
+# env: edgebundling
 
 # https://datashader.org/user_guide/Networks.html#edge-rendering-bundling
 # https://stackoverflow.com/questions/61333273/how-to-use-edge-bundling-with-networkx-and-matplotlib-in-python\
+# https://datashader.org/_modules/datashader/bundling.html
 
+import os
+import math
+import numpy as np
+import pandas as pd
+import datashader as ds
+import datashader.transfer_functions as tf
+from datashader.layout import random_layout, circular_layout, forceatlas2_layout
+from datashader.bundling import connect_edges, hammer_bundle
+from itertools import chain
+import skimage
+from skimage.filters import gaussian, sobel_h, sobel_v
+import matplotlib.pyplot as plt
+
+
+root = r'C:\Users\cristianij\Documents\Projects\edgebundling'
+nodes_csv = os.path.join(root, 'spatial/nodes.csv')
+conns = os.path.join(root, 'spatial/edges_pld{}.csv')
+plds = ['1', '3', '7', '10', '21', '30', '40', '60']
+
+# read in nodes
+nodes = pd.read_csv(nodes_csv)
+
+# hammer_bundle on connections
+for pld in plds:
+    #pld='60'
+    conn = conns.format(pld)
+    conn = pd.read_csv(conn)
+    df = hammer_bundle(nodes, conn, weight='weight', initial_bandwidth=0.05, decay=0.7) # play around with these values
+    break
+
+# Test visualizing with matplotlib
+df.plot(x="x", y="y", figsize=(9,9))
+
+# Test symbolizing by different bundle
+hbnp = df.to_numpy()
+splits = (np.isnan(hbnp[:,0])).nonzero()[0]
+start = 0
+segments = []
+for stop in splits:
+    seg = hbnp[start:stop, :]
+    segments.append(seg)
+    start = stop
+
+fig, ax = plt.subplots(figsize=(7,7))
+#for seg in segments[::50]: # be every 50th segment
+for seg in segments:
+    ax.plot(seg[:,0], seg[:,1])
+
+
+# So it seems like edgebundling doesn't leave separate components. They HAVE to all be connected
+# by definition, even if by only one small connection.
+# Keep in mind, this is about symbolizing graph structure, it does't know things like land barriers.
+
+# Therefore, for my purposes, I don't think this helps me. I think it would just add confusion,
+# even if technically those stepping stone connections are there.
+# It looks like this function is still under development though, so maybe it will be useful in the
+# future.
