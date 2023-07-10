33ad0ce9658dbc7199412ca9ec8b63a01d42cb8d
John Cristiani <jcristia10@gmail.com>
2023-07-10 09:15:12 -0700

    initial

---
+++ dataprep.py
@@ -0,0 +1,4 @@
+
+
+# https://datashader.org/user_guide/Networks.html#edge-rendering-bundling
+# https://stackoverflow.com/questions/61333273/how-to-use-edge-bundling-with-networkx-and-matplotlib-in-python
---
+++ edgebundling.py
@@ -0,0 +1,5 @@
+
+
+# https://datashader.org/user_guide/Networks.html#edge-rendering-bundling
+# https://stackoverflow.com/questions/61333273/how-to-use-edge-bundling-with-networkx-and-matplotlib-in-python\
+
